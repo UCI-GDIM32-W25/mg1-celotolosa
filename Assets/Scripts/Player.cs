7d0f6a1d05b3a645c787f4a65892da2434a30271
celotolosa <mttolosa@uci.edu>
2026-01-07 17:35:07 -0800

    push1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+using UnityEditor;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -13,16 +15,25 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsPlanted = _numSeedsLeft + _numSeeds;
+        _numSeedsLeft = _numSeeds - _numSeedsPlanted;
     }
 
     private void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         
+        _playerTransform.Translate(horizontal, vertical, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantSeed();
+        }
     }
 
     public void PlantSeed ()
     {
-        
+        Instantiate(_plantPrefab, _playerTransform.position, _playerTransform.rotation);
     }
 }
